510035096ebc6a2b7a3e7af53e41379f6f12b5bd
Arne Rutjes <arne123@gmail.com>
2026-06-24 13:10:21 +0200

    prepare docker images for multi-arch build
    example:
    
    docker buildx build --platform linux/amd64,linux/arm64 -t my.docker/repo/fabric-explorer-ui:latest -f ui/Dockerfile --push ui
    docker buildx build --platform linux/amd64,linux/arm64 -t my.docker/repo/fabric-explorer-backend:latest -f Dockerfile --push .
    
    Signed-off-by: Arne Rutjes <arne123@gmail.com>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
-  async rewrites() {
-    const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${backendUrl}/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
